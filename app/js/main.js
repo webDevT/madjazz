cc51f59652a5015a1ea9ec2c520713c65610bc16
webDevT <karinadesyatnik27@gmail.com>
2024-05-26 11:40:54 +0300

    updates

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,21 @@
 $(function(){
 
 $('.leave-feedback').click(function(){
-    $('#leave-feedback').fadeIn();
+    $('#sign-in').fadeIn();
+})
+
+$('#open-register').click(function(){
+    $('#register').fadeIn();
+    $('#sign-in').fadeOut();
+})
+
+$('#open-signin').click(function(){
+    $('#register').fadeOut();
+    $('#sign-in').fadeIn();
+})
+
+$('.dashboard__section-item').click(function(){
+    $(this).toggleClass('active');
 })
 
 });
@@ -26,5 +40,22 @@ $('.instrument-review-modal').click(function(){
 $('#create-instrument-btn').click(function(){
     $('#add-instrument-modal').fadeIn();
 })
+
+
+document.querySelectorAll('.reviews-col').forEach(col => {
+    const popup = col.querySelector('.rating-popup');
+    
+    col.addEventListener('mouseenter', () => {
+        popup.classList.remove('hide-popup');
+        popup.style.pointerEvents = 'auto';
+    });
+
+    col.addEventListener('mouseleave', () => {
+        popup.classList.add('hide-popup');
+        setTimeout(() => {
+            popup.style.pointerEvents = 'none';
+        }, 500); // 2500ms = 2s (задержка) + 0.5s (время анимации)
+    });
+});
 
 
